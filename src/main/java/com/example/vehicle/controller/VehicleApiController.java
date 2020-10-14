7f16acdacfc9042ea46c849eea0e4cd00a9a0486
남궁성환 <saturn203@naver.com>
2020-10-14 22:37:20 +0900

    swagger add

--- src/main/java/com/example/vehicle/controller/VehicleApiController.java
+++ src/main/java/com/example/vehicle/controller/VehicleApiController.java
@@ -9,11 +9,14 @@ import com.example.vehicle.repository.VehicleRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,11 +25,17 @@ import lombok.RequiredArgsConstructor;
 public class VehicleApiController {
     private final VehicleRepository repository;
 
+    @Operation(summary = "차량 정보 전부를 가져온다")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "차량 정보 전부를 가져온다",
+					content = { @Content(mediaType = "application/json",
+							schema = @Schema(implementation = List.class)) }) })
     @GetMapping()
     public List<Vehicle> searchVehicleList(){
         List<Vehicle> vehicleList = repository.findAll();
         return vehicleList;
     }
+
 
     private ResponseEntity<ResultMessage> getResponseEntity(int result) {
 		ResultMessage resultMessage;
--- src/main/java/com/example/vehicle/model/Vehicle.java
+++ src/main/java/com/example/vehicle/model/Vehicle.java
@@ -1,14 +1,13 @@
 package com.example.vehicle.model;
 
-import java.time.LocalDateTime;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
@@ -18,19 +17,31 @@ public class Vehicle {
 
     @Id
 	@Column(name = "object_id")
+    @Schema(description = "오브젝트_ID")
     private String objectId;
-    @Column(name = "year")
-    private String year;
-    @Column(name = "make")
-    private String make;
-    @Column(name = "model")
-    private String model ;
-    @Column(name = "category")
-    private String category;
-    @Column(name = "created_at")   
-    private String createdAt;
-    @Column(name = "updated_at")
-    private String updatedAt;
 
+    @Column(name = "year")
+    @Schema(description = "제조년")
+    private String year;
+
+    @Column(name = "make")
+    @Schema(description = "제조사")
+    private String make;
+
+    @Column(name = "model")
+    @Schema(description = "차량명")
+    private String model ;
+
+    @Column(name = "category")
+    @Schema(description = "카테고리")
+    private String category;
+
+    @Column(name = "created_at")
+    @Schema(description = "데이터 생성일")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    @Schema(description = "데이터 수정일")
+    private String updatedAt;
     
 }
