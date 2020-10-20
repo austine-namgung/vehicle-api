4279ddaa4cbdda0a8d0ec596d593f3bdd94c4f0c
남궁성환 <saturn203@naver.com>
2020-10-20 23:45:04 +0900

    dsfs

--- src/main/java/com/example/vehicle/controller/VehicleApiController.java
+++ src/main/java/com/example/vehicle/controller/VehicleApiController.java
@@ -9,9 +9,8 @@ import com.example.vehicle.repository.VehicleRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +25,12 @@ public class VehicleApiController {
     public List<Vehicle> searchVehicleList(){
         List<Vehicle> vehicleList = repository.findAll();
         return vehicleList;
+    }
+
+	@GetMapping("/vehicle-server")   
+    @ResponseBody 
+    public String  version(){
+        return "this is vehicle-api   #1 server";
     }
 
     private ResponseEntity<ResultMessage> getResponseEntity(int result) {
